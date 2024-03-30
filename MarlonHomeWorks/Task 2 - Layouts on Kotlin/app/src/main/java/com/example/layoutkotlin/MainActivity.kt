cdb90fa04436f7eddde68c416ea685fed465e493
MarlonPrado <marlonstivenprod@ufps.edu.co>
2024-03-29 22:42:42 -0500

    feat - Task #2 - Layouts on Kotlin By Marlon is finished

--- MarlonHomeWorks/Task 2 - Layouts on Kotlin/app/src/main/java/com/example/layoutkotlin/MainActivity.kt
+++ MarlonHomeWorks/Task 2 - Layouts on Kotlin/app/src/main/java/com/example/layoutkotlin/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.layoutkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.layoutkotlin.ui.theme.LayoutKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingPreview()
                 }
             }
         }
@@ -30,17 +35,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun GreetingText(message: String,message2: String,message3: String, modifier: Modifier = Modifier) {
+        Column{
+            Image(painter = painterResource(id = R.drawable.images), contentDescription = message)
+            Text(
+                text = message2,
+                fontSize = 40.sp,
+                lineHeight = 108.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = message3,
+                fontSize = 60.sp,
+                lineHeight = 108.sp,
+                textAlign = TextAlign.Right
+            )
+
+        }
+    }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LayoutKotlinTheme {
-        Greeting("Android")
+        GreetingText("A Silly cat","Hi everyone","Look a cat! Cool !!")
     }
 }
