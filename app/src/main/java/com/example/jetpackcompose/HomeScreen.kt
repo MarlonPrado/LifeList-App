7d8f844f155717ea9e12008125574fdc375701f6
MarlonPrado <marlonstivenprod@ufps.edu.co>
2024-04-21 11:47:13 -0500

    feat - Add a top bar with the name and logo of the app as well as the name of the activity Tasks

--- app/src/main/java/com/example/jetpackcompose/HomeScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/HomeScreen.kt
@@ -40,15 +40,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.db.Task
 import com.example.jetpackcompose.db.task
 
 @Composable
 fun HomeScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize()
     ) {
         WoofTopAppBar()
     }
@@ -68,13 +67,13 @@ fun WoofApp() {
     ) { it ->
         LazyColumn(contentPadding = it) {
             //items(dogs) {
-              //  DogItem(
-                //    dog = it,
-                  //  modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
-                //)
-            }
+            //  DogItem(
+            //    dog = it,
+            //  modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+            //)
         }
     }
+}
 
 
 /**
@@ -87,23 +86,23 @@ fun WoofApp() {
 /**
 @Composable
 fun DogItem(
-    dog: Dog,
-    modifier: Modifier = Modifier
+dog: Dog,
+modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
-        ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
-        }
-    }
+Card(
+modifier = modifier
+) {
+Row(
+modifier = Modifier
+.fillMaxWidth()
+.padding(dimensionResource(R.dimen.padding_small))
+) {
+DogIcon(dog.imageResourceId)
+DogInformation(dog.name, dog.age)
 }
-*/
+}
+}
+ */
 
 /**
  * Composable that displays a Top Bar with an icon and text.
@@ -118,8 +117,14 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                Image(
+                    modifier = Modifier
+                        .size(88.dp)
+                        .padding(8.dp),
+                    painter = painterResource(R.drawable.ic_app),
+                    contentDescription = null)
                 Text(
-                    text = "A Cool Blank Space like My mind"
+                    text = "Life List - Tasks"
                 )
             }
         },
@@ -127,67 +132,24 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
     )
 }
 
-/**
- * Composable that displays a photo of a dog.
- *
- * @param dogIcon is the resource ID for the image of the dog
- * @param modifier modifiers to set to this composable
- */
-
-/**
-@Composable
-fun DogIcon(
-    @DrawableRes dogIcon: Int,
-    modifier: Modifier = Modifier
-) {
-    Image(
-        modifier = modifier
-            .size(dimensionResource(R.dimen.image_size))
-            .padding(dimensionResource(R.dimen.padding_small))
-            .clip(MaterialTheme.shapes.small),
-        contentScale = ContentScale.Crop,
-        painter = painterResource(dogIcon),
-
-        // Content Description is not needed here - image is decorative, and setting a null content
-        // description allows accessibility services to skip this element during navigation.
-
-        contentDescription = null
-    )
-}
-*/
-
-/**
- * Composable that displays a dog's name and age.
- *
- * @param dogName is the resource ID for the string of the dog's name
- * @param dogAge is the Int that represents the dog's age
- * @param modifier modifiers to set to this composable
- */
 
 
-/**
+
 @Composable
 fun DogInformation(
-    @StringRes dogName: Int,
-    dogAge: Int,
-    modifier: Modifier = Modifier
+@StringRes dogName: Int,
+dogAge: Int,
+modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = stringResource(dogName),
-            style = MaterialTheme.typography.displayMedium,
-            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
-        )
-        Text(
-            text = stringResource(R.string.years_old, dogAge),
-            style = MaterialTheme.typography.bodyLarge
-        )
-    }
+Column(modifier = modifier) {
+Text(
+text = stringResource(dogName),
+style = MaterialTheme.typography.displayMedium
+)
+Text(   //R.string.years_old
+text = stringResource(dogAge),
+style = MaterialTheme.typography.bodyLarge
+)
 }
-
-*/
-
-/**
- * Composable that displays what the UI of the app looks like in light theme in the design tab.
- */
+}
 
--- app/src/main/java/com/example/jetpackcompose/db/Task.kt
+++ app/src/main/java/com/example/jetpackcompose/db/Task.kt
@@ -1,19 +1,37 @@
 package com.example.jetpackcompose.db
 
+import androidx.annotation.StringRes
+import com.example.jetpackcompose.R
 data class Task(
     val id: Int,
-    val name: String,
+    @StringRes val name: Int,
     val createdDate: String
 )
 
 val task = listOf(
-    Task(1, "Fetch the ball", "2023-04-20"),
-    Task(2, "Enjoy a relaxing nap", "2023-04-19"),
-    Task(3, "Play with squeaky toys", "2023-04-18"),
-    Task(4, "Go for a walk", "2023-04-17"),
-    Task(5, "Cuddle on the couch", "2023-04-16"),
-    Task(6, "Enjoy a tasty treat", "2023-04-15"),
-    Task(7, "Play in the backyard", "2023-04-14"),
-    Task(8, "Go to the dog park", "2023-04-13"),
-    Task(9, "Learn a new trick", "2023-04-12")
+    Task(1, R.string.fetch_the_ball, "2023-04-20"),
+    Task(2, R.string.enjoy_a_relaxing_nap, "2023-04-19"),
+    Task(3, R.string.play_with_squeaky_toys, "2023-04-18"),
+    Task(4, R.string.go_for_a_walk, "2023-04-17"),
+    Task(5, R.string.cuddle_on_the_couch, "2023-04-16"),
+    Task(6, R.string.enjoy_a_tasty_treat, "2023-04-15"),
+    Task(7, R.string.play_in_the_backyard, "2023-04-14"),
+    Task(8, R.string.go_to_the_dog_park, "2023-04-13"),
+    Task(9, R.string.learn_a_new_trick, "2023-04-12"),
+    Task(10,R.string.take_a_bath, "2023-04-11"),
+    Task(11,R.string.play_hide_and_seek, "2023-04-10"),
+    Task(12, R.string.go_for_a_car_ride, "2023-04-09"),
+    Task(13, R.string.chase_squirrels, "2023-04-08"),
+    Task(14, R.string.dig_in_the_garden, "2023-04-07"),
+    Task(15, R.string.guard_the_house, "2023-04-06"),
+    Task(16, R.string.play_tug_of_war, "2023-04-05"),
+    Task(17, R.string.attend_obedience_class, "2023-04-04"),
+    Task(18, R.string.visit_the_groomer, "2023-04-03"),
+    Task(19, R.string.learn_a_new_command, "2023-04-02"),
+    Task(20, R.string.explore_a_new_trail, "2023-04-01"),
+    Task(21, R.string.attend_a_doggy_birthday_party, "2023-03-22"),
+    Task(22, R.string.learn_to_play_dead, "2023-03-21"),
+    Task(23, R.string.practice_recall_training, "2023-03-20"),
+    Task(24, R.string.go_camping_with_your_human, "2023-03-19"),
+    Task(25, R.string.visit_a_dog_friendly_restaurant, "2023-03-18")
 )
